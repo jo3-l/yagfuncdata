eb9d080b3444217fe3823970f2b58d9f08389f79
Luca Zeuch <l-zeuch@email.de>
2024-06-24 21:44:05 +0200

    feat(lytfs): allow fetching from repo and branch (#3)
    * feat(lytfs): allow fetching from repo and branch
    
    Allow specifying a target repository and branch to fetch from.
    
    This may be useful for users running a selfhosted version of YAGPDB and
    wanting to grab their instance's functions list, or just the development
    version of the upstream repository.
    
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>
    
    * fixup! feat(lytfs): allow fetching from repo and branch
    
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>
    
    * style(lytfs): use owner/repo@branch format instead
    
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>
    
    ---------
    
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>

--- cmd/lytfs/main.go
+++ cmd/lytfs/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
 	"time"
 
 	"github.com/google/go-github/v55/github"
@@ -12,13 +13,13 @@ import (
 )
 
 var (
-	timeout = flag.Duration("timeout", 5*time.Second, "timeout for fetching data; default: 5s")
+	timeout = flag.Duration("timeout", 5*time.Second, "timeout for fetching data.")
 )
 
 func usage() {
 	fmt.Fprintln(os.Stderr, `lytfs: list available YAGPDB template function names
 	
-usage: lytfs [-timeout duration]
+usage: lytfs [owner/repo@branch] [-timeout duration]
 
 To authenticate your requests, pass a GitHub personal access token via the LYTFS_GITHUB_TOKEN environment variable.`)
 	flag.PrintDefaults()
@@ -31,10 +32,30 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
 	defer cancel()
 
-	fcp := yagfuncdata.DefaultFileContentProvider
-	if token := os.Getenv("LYTFS_GITHUB_TOKEN"); token != "" {
-		fcp = yagfuncdata.NewGitHubFileProvider(github.NewClient(nil).WithAuthToken(token), "botlabs-gg", "yagpdb", "master")
+	owner := "botlabs-gg"
+	repo := "yagpdb"
+	branch := "master"
+
+	target := flag.Arg(0)
+	if target != "" {
+		var targetRegex = regexp.MustCompile(`^([^/]+)/([^@]+)@(.+)$`)
+		matches := targetRegex.FindStringSubmatch(target)
+		if len(matches) != 4 {
+			fmt.Fprintln(os.Stderr, "invalid target format")
+			os.Exit(1)
+		}
+
+		owner = matches[1]
+		repo = matches[2]
+		branch = matches[3]
 	}
+
+	fcp := yagfuncdata.NewGitHubFileProvider(github.NewClient(nil), owner, repo, branch)
+
+	if token := os.Getenv("LYTFS_GITHUB_TOKEN"); token != "" {
+		fcp = yagfuncdata.NewGitHubFileProvider(github.NewClient(nil).WithAuthToken(token), owner, repo, branch)
+	}
+
 	sources := yagfuncdata.DefaultSources(fcp)
 	funcs, err := yagfuncdata.Fetch(ctx, sources)
 	if err != nil {
