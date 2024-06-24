0d379262091dd0092acbf262519cc6be7d6058ef
Luca Zeuch <l-zeuch@email.de>
2024-06-24 12:04:12 +0200

    feat(lytfs): allow fetching from repo and branch
    Allow specifying a target repository and branch to fetch from.
    
    This may be useful for users running a selfhosted version of YAGPDB and
    wanting to grab their instance's functions list, or just the development
    version of the upstream repository.
    
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>

--- cmd/lytfs/main.go
+++ cmd/lytfs/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/google/go-github/v55/github"
@@ -12,13 +13,15 @@ import (
 )
 
 var (
-	timeout = flag.Duration("timeout", 5*time.Second, "timeout for fetching data; default: 5s")
+	timeout = flag.Duration("timeout", 5*time.Second, "timeout for fetching data.")
+	repo    = flag.String("repo", "botlabs-gg/yagpdb", "GitHub repository to fetch data from.")
+	branch  = flag.String("branch", "master", "GitHub branch to fetch data from.")
 )
 
 func usage() {
 	fmt.Fprintln(os.Stderr, `lytfs: list available YAGPDB template function names
 	
-usage: lytfs [-timeout duration]
+usage: lytfs [-timeout duration] [-repo owner/repo] [-branch branch]
 
 To authenticate your requests, pass a GitHub personal access token via the LYTFS_GITHUB_TOKEN environment variable.`)
 	flag.PrintDefaults()
@@ -31,9 +34,12 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
 	defer cancel()
 
+	owner := strings.Split(*repo, "/")[0]
+	repo := strings.Split(*repo, "/")[1]
+
 	fcp := yagfuncdata.DefaultFileContentProvider
 	if token := os.Getenv("LYTFS_GITHUB_TOKEN"); token != "" {
-		fcp = yagfuncdata.NewGitHubFileProvider(github.NewClient(nil).WithAuthToken(token), "botlabs-gg", "yagpdb", "master")
+		fcp = yagfuncdata.NewGitHubFileProvider(github.NewClient(nil).WithAuthToken(token), owner, repo, *branch)
 	}
 	sources := yagfuncdata.DefaultSources(fcp)
 	funcs, err := yagfuncdata.Fetch(ctx, sources)
